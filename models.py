5f0afc7b3786af194ab8a5a07c4b76da758130a4
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2024-07-05 16:16:43 -0700

    added more models

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy.stats import loguniform, expon, randint, uniform
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier, Perceptron
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 
 
@@ -79,6 +82,53 @@ POSSIBLE_CLASSIFIERS = {
             'decision_function_shape': ['ovo', 'ovr'],
             'break_ties': [True, False],
             'random_state': randint(0, 100)
+        }
+    },
+    'MultinomialNB': {
+        'model': MultinomialNB(),
+        'params': {
+            'alpha': np.linspace(0.1, 2, 20),  # Additive (Laplace/Lidstone) smoothing parameter
+            'fit_prior': [True, False]  # Whether to learn class prior probabilities or not.
+        }
+    },
+    'DecisionTreeClassifier': {
+        'model': DecisionTreeClassifier(random_state=42),
+        'params': {
+            'criterion': ['gini', 'entropy'],  # Function to measure the quality of a split
+            'max_depth': [None, 10, 20, 30, 40, 50],  # Maximum depth of the tree
+            'min_samples_split': [2, 5, 10],  # Minimum number of samples required to split an internal node
+            'min_samples_leaf': [1, 2, 4],  # Minimum number of samples required to be at a leaf node
+            'max_features': ['auto', 'sqrt', 'log2']  # Number of features to consider when looking for the best split
+        }
+    },
+    'KNeighborsClassifier': {
+        'model': KNeighborsClassifier(),
+        'params': {
+            'n_neighbors': [3, 5, 7, 9],  # Number of neighbors to use
+            'weights': ['uniform', 'distance'],  # Weight function used in prediction
+            'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],  # Algorithm used to compute the nearest neighbors
+            'p': [1, 2]  # Power parameter for the Minkowski metric
+        }
+    },
+    'SGDClassifier': {
+        'model': SGDClassifier(random_state=42, max_iter=1000),
+        'params': {
+            'loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'],  # Loss function to be used
+            'penalty': ['l2', 'l1', 'elasticnet'],  # The penalty (aka regularization term) to be used
+            'alpha': np.logspace(-6, -1, 10),  # Constant that multiplies the regularization term
+            'learning_rate': ['constant', 'optimal', 'invscaling', 'adaptive'],  # Learning rate schedule
+            'eta0': [0.01, 0.1, 1]  # Initial learning rate for the 'constant', 'invscaling' or 'adaptive' schedules
+        }
+    },
+    'Perceptron': {
+        'model': Perceptron(random_state=42, max_iter=1000),
+        'params': {
+            'penalty': [None, 'l2', 'l1', 'elasticnet'],  # The penalty (aka regularization term) to be used
+            'alpha': np.logspace(-6, -1, 10),  # Constant that multiplies the regularization term
+            'fit_intercept': [True, False],  # Whether the intercept should be estimated or not
+            'shuffle': [True, False],  # Whether or not the training data should be shuffled after each epoch
+            'eta0': [0.01, 0.1, 1],  # Constant by which the updates are multiplied
+            'warm_start': [True, False]  # Whether to reuse the solution of the previous call to fit as initialization
         }
     }
 }
